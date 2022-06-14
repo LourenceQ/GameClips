55d634ba8c7bca63c4b5130fad1dc21beb776657
LourenceQ <lqfmecatronica@gmail.com>
2022-06-14 17:01:52 -0300

    Rearranging modla components

--- GameClips/src/app/user/user.module.ts
+++ GameClips/src/app/user/user.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     AuthModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     AuthModalComponent
