1d51b6bd030ceb7ac654d35ac69df7c832e0ef7a
LourenceQ <lqfmecatronica@gmail.com>
2022-06-14 16:10:25 -0300

    Creating user module, componenets for header and modal

---
+++ GameClips/src/app/user/user.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
+
+
+
+@NgModule({
+  declarations: [
+    AuthModalComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AuthModalComponent
+  ]
+})
+export class UserModule { }
