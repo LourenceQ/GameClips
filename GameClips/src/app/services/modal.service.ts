d9293644e76b899c7a4a22dc174a50638c46cfe9
LourenceQ <lqfmecatronica@gmail.com>
2022-06-15 15:39:13 -0300

    refatorando metodos de modalService para tratar modais de forma separada

--- GameClips/src/app/services/modal.service.ts
+++ GameClips/src/app/services/modal.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string
+  visible: boolean
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
-  private visible = false
+  private modals: IModal[] = []
 
   constructor() { }
 
-  isModalOpen() {
-    return this.visible
+  registerModal(id: string) {
+    this.modals.push({
+      id,
+      visible: false
+    })
   }
 
-  toggleModal() {
-    this.visible = !this.visible
+  isModalOpen(id: string) : boolean {
+    return Boolean(this.modals.find(el => el.id === id)?.visible)    
+  }
+
+  toggleModal(id: string) {
+    const modal = this.modals.find(el => el.id === id)
+
+    if(modal) {
+      modal.visible = ! modal.visible
+    }    
   }
 }
