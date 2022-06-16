ab5e26014068f515e135bc7f386579cc57ee80ea
LourenceQ <lqfmecatronica@gmail.com>
2022-06-16 08:25:28 -0300

    Usando objeto elementRef como wrapper para colocar <app-auth-modal> dentro de body tag

--- GameClips/src/app/shared/modal/modal.component.ts
+++ GameClips/src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -9,9 +9,13 @@ import { ModalService } from 'src/app/services/modal.service';
 export class ModalComponent implements OnInit {
   @Input() modalId = ''
 
-  constructor(public modal: ModalService) { }
+  constructor(
+    public modal: ModalService
+    , public elementRef: ElementRef
+    ) { }
 
   ngOnInit(): void {
+    document.body.appendChild(this.elementRef.nativeElement)
   }
 
   closeModal() {
