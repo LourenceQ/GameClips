b9fb8723d0bbc4ee017c5f84b4421e6ad0e9e058
LourenceQ <lqfmecatronica@gmail.com>
2022-06-16 09:43:44 -0300

    Implementando funcionalidade de acionar tabs com click

--- GameClips/src/app/shared/tabs-container/tabs-container.component.ts
+++ GameClips/src/app/shared/tabs-container/tabs-container.component.ts
@@ -12,6 +12,19 @@ export class TabsContainerComponent implements AfterContentInit {
   constructor() { }
 
   ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter( tab => tab.active)
+    
+    if(!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first)
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(el => {
+      el.active = false
+    })
+
+    tab.active = true
   }
 
 }
