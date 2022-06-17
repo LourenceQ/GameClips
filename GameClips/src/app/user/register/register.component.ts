41f0d8b7e4c37916df19212dbe038106d16725f5
LourenceQ <lqfmecatronica@gmail.com>
2022-06-17 12:55:15 -0300

    Criando input component pra ser reutizável

--- GameClips/src/app/user/register/register.component.ts
+++ GameClips/src/app/user/register/register.component.ts
@@ -6,16 +6,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent   {
-  registerForm = new FormGroup({
-    name: new FormControl('', [
-      Validators.required
-    ]),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
-    phoneNumber: new FormControl('')
-  })
+export class RegisterComponent   {  
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ])
+  email = new FormControl('')
+  age = new FormControl('')
+  password = new FormControl('')
+  confirm_password = new FormControl('')
+  phoneNumber = new FormControl('')
 
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    pasword: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
+  })
+  
 }
