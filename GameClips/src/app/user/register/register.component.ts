87b2695dae002d905b8da8fcd3b32105126f9dfe
LourenceQ <lqfmecatronica@gmail.com>
2022-06-17 09:44:57 -0300

    Implementando FormGroup no component de register e autenticação

--- GameClips/src/app/user/register/register.component.ts
+++ GameClips/src/app/user/register/register.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'; 
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class RegisterComponent   {
+  registerForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required
+    ]),
+    email: new FormControl(''),
+    age: new FormControl(''),
+    password: new FormControl(''),
+    confirm_password: new FormControl(''),
+    phoneNumber: new FormControl('')
+  })
 
 }
--- GameClips/src/app/user/user.module.ts
+++ GameClips/src/app/user/user.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports: [
     AuthModalComponent
