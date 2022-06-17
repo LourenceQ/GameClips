84d19ea686a570200fbdd8acb3a698a52cd6bc71
LourenceQ <lqfmecatronica@gmail.com>
2022-06-17 09:01:37 -0300

    criando componentes para separar login e register forms

--- GameClips/src/app/user/user.module.ts
+++ GameClips/src/app/user/user.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AuthModalComponent
+    AuthModalComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
