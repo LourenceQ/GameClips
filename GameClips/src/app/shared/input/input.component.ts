984881be61259f99c84c2542c8af25623b6f6a1e
LourenceQ <lqfmecatronica@gmail.com>
2022-06-20 14:28:07 -0300

    Implementando validators em name, age, password

--- GameClips/src/app/shared/input/input.component.ts
+++ GameClips/src/app/shared/input/input.component.ts
@@ -8,6 +8,9 @@ import { FormControl } from '@angular/forms';
 })
 export class InputComponent implements OnInit {
   @Input() control: FormControl = new FormControl()
+  @Input() type = 'text'
+  @Input() placeholder = ''
+
   constructor() { }
 
   ngOnInit(): void {
