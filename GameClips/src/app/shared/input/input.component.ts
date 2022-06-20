05077a3b1d26144e7cfa713d09996c41b49a7725
LourenceQ <lqfmecatronica@gmail.com>
2022-06-20 14:43:15 -0300

    Implementand ngx-mask

--- GameClips/src/app/shared/input/input.component.ts
+++ GameClips/src/app/shared/input/input.component.ts
@@ -10,6 +10,7 @@ export class InputComponent implements OnInit {
   @Input() control: FormControl = new FormControl()
   @Input() type = 'text'
   @Input() placeholder = ''
+  @Input() format =''
 
   constructor() { }
 
