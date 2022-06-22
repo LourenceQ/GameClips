b71da2072530aeaeee3e217695dd196098bb4852
LourenceQ <lqfmecatronica@gmail.com>
2022-06-21 22:58:28 -0300

    finishing input validation

--- GameClips/src/app/user/register/register.component.ts
+++ GameClips/src/app/user/register/register.component.ts
@@ -27,7 +27,11 @@ export class RegisterComponent   {
   confirm_password = new FormControl('', [
     Validators.required
   ])
-  phoneNumber = new FormControl('')
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(14),
+    Validators.maxLength(14)
+  ])
 
   registerForm = new FormGroup({
     name: this.name,
